7639c64c814be4f146452facc80eca7e5c14f27d
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-03-17 19:13:06 +0200

    Cleanup cache implementation

--- cache.ts
+++ cache.ts
@@ -1,25 +1,18 @@
 import * as SecureStore from "expo-secure-store";
 import { Platform } from "react-native";
+import { TokenCache } from "@clerk/clerk-expo/dist/cache";
 
-//SecureStore.deleteItemAsync("__clerk_client_jwt");
-
-export async function saveToken(key: string, value: string) {
-  // console.log("Save token", key, value);
-  await SecureStore.setItemAsync(key, value);
-}
-
-export async function getToken(key: string) {
-  const value = await SecureStore.getItemAsync(key);
-  // console.log("Get token", key, value);
-  return value;
-}
+const createTokenCache = (): TokenCache => {
+    return {
+        getToken: (key) => {
+            return SecureStore.getItemAsync(key);
+        },
+        saveToken: (key, token) => {
+            return SecureStore.setItemAsync(key, token);
+        },
+    };
+};
 
 // SecureStore is not supported on the web
 // https://github.com/expo/expo/issues/7744#issuecomment-611093485
-export const tokenCache =
-  Platform.OS !== "web"
-    ? {
-        getToken,
-        saveToken,
-      }
-    : undefined;
+export const tokenCache = Platform.OS !== "web" ? createTokenCache() : undefined;
